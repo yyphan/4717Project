5e0fcc7033a573aeb2f049d3741f6aed6b198805
dariuscmh <79561848+dariuscmh@users.noreply.github.com>
2021-10-18 20:49:06 +0800

    Add files via upload
    Part 1 added except menu price

---
+++ CaseStudy/Price_update.php
@@ -0,0 +1,79 @@
+<?php
+include "fetch.php";
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+	 <title>JavaJam Coffee House</title>
+     <meta charset="utf-8">
+     <link rel="stylesheet" href="css/menu.css">
+	 <script defer src="update.js"></script>
+</head>
+<body>
+	<div class="wrapper">
+<header>
+     <img src="assets/header.png">
+</header>
+        <div id="LeftColumn">
+            <nav>
+                <a href="admin.php">Go back</a>
+            </nav>
+        </div>
+
+			<div id="RightColumn">
+            <h2>Click to update product prices</h2>
+			<form action="UpdateDB.php" method="get">
+			
+            <table id="MenuTable">
+                <tr>
+					<td class="width:50px;"><input type="checkbox" name="JavaCheck" id="JavaCheck" onclick="updateInput(1)">
+                    <td class="title"><strong>Just Java</strong></td>
+                    <td class="description">
+                        Regular house blend, decaffeinated coffee, or flavor of the day.
+                        <br>
+                        <strong>Endless Cup $<span id="JavaPrice"></span><input style="width:50px;" step="0.01" formnovalidate type="hidden" id="Java" name="Java"></strong>
+									<?php insert_row(1) ?>
+                   
+                    </td>
+                    
+                </tr>
+                <tr>
+					<td class="width:50px;"><input type="checkbox" name="cafeCheck" id="cafeCheck" onclick="updateInput(2)">
+                    <td class="title"><strong>Cafe au Lait</strong></td>
+                    <td class="description">
+                        House blended coffee infused into a smooth, steamed milk.
+                        <br>
+                        <strong>Single $<span id="singleCafePrice"></span><input style="width: 50px;" step="0.01" type="hidden" id="singleCafe" name="singleCafe"></strong>
+						<?php insert_row(2) ?>
+                        
+                        <strong>Double $<span id="doubleCafePrice"></span><input style="width: 50px;" step="0.01" type="hidden" id="doubleCafe" name="doubleCafe"></strong>
+						<?php insert_row(3) ?>
+                    </td>
+                    
+                </tr>
+                <tr>
+					<td class="width:50px;"><input type="checkbox" name="cappuccinoCheck" id="cappuccinoCheck" onclick="updateInput(3)">
+                    <td class="title"><strong>Iced Cappuccino</strong></td>
+                    <td class="description">
+                        Sweetened espresso blended with icy-cold milk and served in a chilled glass.
+                        <br>
+                        <strong>Single $<span id="singleCappuccinoPrice"></span><input style="width: 50px;" step="0.01" type="hidden" id="singleCappuccino" name="singleCappuccino"></strong>
+						 <?php insert_row(4) ?>
+                        <strong>Double $<span id="doubleCappuccinoPrice"></span><input style="width: 50px;" step="0.01" type="hidden" id="doubleCappuccino" name="doubleCappuccino"></strong>
+						 <?php insert_row(5) ?>
+                    </td>
+                    
+                </tr>
+               
+            </table>
+        </div>
+        <footer> 
+            Copyright &copy; 2021 JavaJam Coffee House
+            <br>
+            <a href="mailto:yifan@yao.com">yifan@yao.com</a>
+        </footer>
+   
+
+
