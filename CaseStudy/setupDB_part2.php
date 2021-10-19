4d79f3f8a13c855f145126896edec5e9de5fd6fe
yyphan <jeromeyao1999@gmail.com>
2021-10-19 17:16:13 +0800

    total sales page done

--- CaseStudy/setupDB_part2.php
+++ CaseStudy/setupDB_part2.php
@@ -26,4 +26,38 @@ if (!mysqli_query($conn, $sql)) {
 }
 
 mysqli_close($conn);
+
+// -------------Do these manually in phpmyadmin----------------
+// -------------created_at will be current date----------------
+
+// INSERT INTO sales (id, product_id, product_price, created_at)
+// VALUES (NULL, '1', 2.00, '2021-10-19');
+// INSERT INTO sales (id, product_id, product_price, created_at)
+// VALUES (NULL, '1', 2.00, '2021-10-19');
+// INSERT INTO sales (id, product_id, product_price, created_at)
+// VALUES (NULL, '2', 3.00, '2021-10-19');
+// INSERT INTO sales (id, product_id, product_price, created_at)
+// VALUES (NULL, '3', 4.00, '2021-10-19');
+// INSERT INTO sales (id, product_id, product_price, created_at)
+// VALUES (NULL, '4', 4.75, '2021-10-19');
+// INSERT INTO sales (id, product_id, product_price, created_at)
+// VALUES (NULL, '2', 3.00, '2021-10-19');
+// INSERT INTO sales (id, product_id, product_price, created_at)
+// VALUES (NULL, '3', 4.00, '2021-10-19');
+// INSERT INTO sales (id, product_id, product_price, created_at)
+// VALUES (NULL, '5', 5.75, '2021-10-19');
+// INSERT INTO sales (id, product_id, product_price, created_at)
+// VALUES (NULL, '1', 2.00, '2021-10-19');
+// INSERT INTO sales (id, product_id, product_price, created_at)
+// VALUES (NULL, '2', 3.00, '2021-10-19');
+// INSERT INTO sales (id, product_id, product_price, created_at)
+// VALUES (NULL, '3', 4.00, '2021-10-19');
+// INSERT INTO sales (id, product_id, product_price, created_at)
+// VALUES (NULL, '5', 5.75, '2021-10-19');
+// INSERT INTO sales (id, product_id, product_price, created_at)
+// VALUES (NULL, '2', 3.00, '2021-10-19');
+// INSERT INTO sales (id, product_id, product_price, created_at)
+// VALUES (NULL, '3', 4.00, '2021-10-19');
+// INSERT INTO sales (id, product_id, product_price, created_at)
+// VALUES (NULL, '5', 5.75, '2021-10-19');
 ?>
