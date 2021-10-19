1f64ef8f4264256fb905a21b7ac914e45fc84578
dariuscmh <79561848+dariuscmh@users.noreply.github.com>
2021-10-20 00:22:54 +0800

    Add files via upload

--- CaseStudy/update.js
+++ CaseStudy/update.js
@@ -2,30 +2,30 @@ function updateInput(id){
     
     switch(id){
         case 1:
-            inputLow = document.getElementById("Java");
+            input1 = document.getElementById("Java");
             if(document.getElementById("JavaCheck").checked){ 
-                inputLow.type = "number";
+                input1.type = "number";
 				document.getElementById("JavaCheck").disabled = true;
             }
         
             break;
         case 2:
-			inputLow = document.getElementById("singleCafe");
-			inputHigh = document.getElementById("doubleCafe");
+			input2 = document.getElementById("singleCafe");
+			input3 = document.getElementById("doubleCafe");
             if(document.getElementById("cafeCheck").checked) {
 				document.getElementById("cafeCheck").disabled = true;
-                inputLow.type = "number";
-                inputHigh.type = "number";
+                input2.type = "number";
+                input3.type = "number";
             }
            
             break;
         case 3:
-            inputLow = document.getElementById("singleCappuccino");
-            inputHigh = document.getElementById("doubleCappuccino");
+            input4 = document.getElementById("singleCappuccino");
+            input5 = document.getElementById("doubleCappuccino");
             if(document.getElementById("cappuccinoCheck").checked){
 				document.getElementById("cappuccinoCheck").disabled = true;
-                inputLow.type = "number";
-                inputHigh.type = "number";
+                input4.type = "number";
+                input5.type = "number";
             }
           
             break;
