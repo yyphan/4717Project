8861e79e0f24d25356884fb788e9de5b77292657
yyphan <jeromeyao1999@gmail.com>
2021-10-27 10:50:41 +0800

    create tables

---
+++ setupDB.php
@@ -0,0 +1,33 @@
+<?php
+include("conn.php");
+
+// Create `users` table 
+$sql = "CREATE TABLE IF NOT EXISTS `users` (
+`id` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+`name` VARCHAR(20) NOT NULL,
+`email` VARCHAR(50) NOT NULL, 
+`password` VARCHAR(150) NOT NULL,
+`role` VARCHAR(20) NOT NULL
+)";
+
+if (!mysqli_query($conn, $sql)) {
+    echo "Error creating users: " . mysqli_error($conn);
+    mysqli_close($conn);
+}
+
+// Create `appointments` table
+$sql = "CREATE TABLE IF NOT EXISTS `appointments` (
+`id` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+`start_at` DATETIME NOT NULL,
+`end_at` DATETIME NOT NULL, 
+`doctor_id` INT NOT NULL,
+`patient_id` INT NOT NULL
+)";
+
+if (!mysqli_query($conn, $sql)) {
+    echo "Error creating appointments: " . mysqli_error($conn);
+    mysqli_close($conn);
+}
+
+mysqli_close($conn);
+?>
