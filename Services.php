a885484335f95e616bcfed147bb45fe8a4d40683
yyphan <jeromeyao1999@gmail.com>
2021-10-31 20:13:28 +0800

    Refactoring

---
+++ Services.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Contact Us</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/common.css">
+    <link rel="stylesheet" href="css/services.css">
+</head>
+
+<body>
+    <div id="wrapper">
+        <?php include("header.php"); ?>
+
+        <div class="service">
+            <img src="assets/service.jpg" height="350" width="auto" style="float: left; padding:90px 20px 20px 20px;">
+            <h1><strong>WELCOME TO M&Y DENTAL CLINIC AT NTU</strong></h1>
+            <p5><strong>Professional Dental Services</strong></p5><br><br><br>
+            <p3><strong>We provide a range of professional dental services to suit your dental needs, <br>including fixing of teeth overcrowding, therapies to ease a toothache, <br>or alleviating jaw joint pain.</strong></p3>
+            <table>
+                <tr>
+                    <th class="tr1"><img src="assets/implant.png">
+                    </th>
+                    <th class="tr1"><img src="assets/wisdom.png">
+                    </th>
+                    <th class="tr1"><img src="assets/rootcanal.png">
+                    </th>
+                    <th class="tr1"><img src="assets/24hour.png">
+                    </th>
+                </tr>
+
+                <tr>
+                    <td class="td1">
+                        <strong>
+                            Dental Implants
+                        </strong>
+                    </td>
+                    <td class="td1">
+                        <strong>
+                            Wisdom Tooth Surgery
+                        </strong></td>
+                    <td class="td1">
+                        <strong>
+                            Root Canal Treatment
+                        </strong></td>
+                    <td class="td1">
+                        <strong>
+                            Emergency Services
+                        </strong></td>
+                </tr>
+
+                <tr>
+                    <td class="td1">Dental implants are a permanent solution to missing teeth or wearing removable dentures.</td>
+                    <td class="td1">Wisdom tooth can erupt and causes discomfort. It often grow in a disorderly manner and sometimes require extractions.</td>
+                    <td class="td1">The pulp in your tooth, consisting of nerves and blood vessels, can become diseased from decay or injury. A root canal treatment involves cleaning out the damaged pulp.</td>
+                    <td class="td1">We have a dedicated 24-hour emergency service on-hand to look after all of your dental needs.</td>
+                </tr>
+
+            </table>
+        </div>
+
+        <footer>
+            Copyright &copy; 2021 M&Y Dental
+            <br>
+            <a href="mailto:MYDental@dental.com">MYDental@dental.com</a>
+        </footer>
+    </div>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,19 @@
+<header>
+    <div id="menubar">
+        <a href="index.php">
+            <img src="assets/dicon1.jpg" height="100" width="auto" style="float: left;">
+        </a>
+
+        <button class="button button1" onclick="window.location='login.php'">Login</button>
+        <button class="button button2" onclick="window.location='register.php'">Register</button>
+
+        <ul>
+            <li><a href="index.php"><strong>Home</strong></a></li>
+            <li><a href="dentists.php"><strong>Dentists</strong></a></li>
+            <li><a href="Services.php"><strong>Our Services</strong></a></li>
+            <li><a href="about.php"><strong>About Us</strong></a></li>
+            <li><a href="contact.php"><strong>Contact Us</strong></a></li>
+            <li><a href="booking.php"><strong>Booking</strong></a></li>
+        </ul>
+    </div>
+</header>
