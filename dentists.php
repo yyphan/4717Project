380a2f785b0aaf667fce359981cdd55738677a2a
dariuscmh <79561848+dariuscmh@users.noreply.github.com>
2021-10-31 21:29:46 +0800

    Add files via upload

--- dentists.php
+++ dentists.php
@@ -11,7 +11,7 @@
 <body>
     <div id="wrapper">
         <?php include("header.php"); ?>
-        <div class="dentist">
+        <div class = "dentist">
             <img src="assets/bender.jpg" height="200" width="auto" style="float: left; padding:90px 20px 20px 20px;">
             <h1><strong>Hi there, </strong></h1>
             <p2><strong>I am Dr Bender Bot</strong></p2><br><br><br>
@@ -24,8 +24,26 @@
                 <li2><strong>Dentium Implants</strong></li2><br>
                 <li2><strong>Osteopore</strong></li2><br>
                 <li2><strong>Nobel Biocare Implants</strong></li2><br>
-
             </ul2>
+			<div id = "cpo">
+			
+			<img src="assets/c3po.jpg" height="200" width="auto" style="float: left; padding-right:10px; padding-top:40px;">
+			
+			<p6><strong>I am Dr Goldman Bot</strong></p6>
+			<p7>Dr Goldman Bot graduated in Dentistry (BDS) from Starwars in 2002 and Medicine (MBBS) from Valda in 2006. He was the first AI bot recipient for the
+                <br>Research Fellowship Award. He spent the final year elective of his medical degree in the Craniofacial Unit at The Hospital for jedis in vale, Warland.
+            </p7>
+			
+			<h2 style="padding-top:130px;"><strong><u>Specialities</u></strong></h2>
+			 <ul2>
+                <li2><strong>Endodontics </strong></li2><br>
+                <li2><strong>Paediatric Dentistry</strong></li2><br>
+                <li2><strong>Oral & Maxillofacial Surgery</strong></li2><br>
+                <li2><strong>Prosthodontics</strong></li2><br>
+            </ul2>
+		
+			
+			</div>
         </div>
 
         <footer>
